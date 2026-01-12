322dd0a0233ded19a96c8d65a77b8fba1a52388e
vaghelayuvrajsinh4190-afk <vaghelayuvrajsinh4190@gmail.com>
2026-01-12 12:38:44 +0530

    Change TOKEN string quotes from single to double

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def keep_alive():
 # ================= CONFIGURATION =================
 # ⚠️ TOKEN IS SECURE VIA ENVIRONMENT VARIABLES
 # Make sure you set 'TOKEN' in your Render Environment Variables!
-TOKEN = os.environ.get('MTQ1OTE2NDczMjAzMTI0MjM0Nw.GOkdIP.HkxafSs-Ew3KaZ3zKnHhK-GnVsJie-zMMCjYRo')
+TOKEN = os.environ.get("MTQ1OTE2NDczMjAzMTI0MjM0Nw.GOkdIP.HkxafSs-Ew3KaZ3zKnHhK-GnVsJie-zMMCjYRo")
 
 # --- CHANNELS ---
 # 1. Admin Command Channel (Where you type !setup, !lock, etc.)
