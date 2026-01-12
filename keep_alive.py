525583b57cf3fe84e8d6b2dffadf9fb8cb878e7b
vaghelayuvrajsinh4190-afk <vaghelayuvrajsinh4190@gmail.com>
2026-01-12 11:40:06 +0530

    Refactor Flask app initialization and response message

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "I am alive! The bot is running."
+    return "Bot is alive!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
