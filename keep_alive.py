733c606a692eb632572268bba8ceb89c3a3b8d78
vaghelayuvrajsinh4190-afk <vaghelayuvrajsinh4190@gmail.com>
2026-01-13 15:02:26 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    # Gets the PORT from the environment (Render/Heroku) or uses 8080
+    # Uses the PORT provided by the hosting service or defaults to 8080
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
